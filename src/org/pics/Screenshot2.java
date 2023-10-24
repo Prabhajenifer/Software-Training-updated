0b1abd2ac8b7f62de8965ae3284f76ed5a494bff
Prabhajenifer <jjenifer1612@gmail.com>
2023-10-24 14:17:17 +0530

    Updated registration functionality

---
+++ src/org/pics/Screenshot2.java
@@ -0,0 +1,23 @@
+package org.pics;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshot2 {
+public static void main(String[] args) throws IOException {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\DELL\\eclipse-workspace\\TakeScreenShot\\Driver\\chromedriver.exe");
+	WebDriver d=new ChromeDriver();
+	d.get("https://en-gb.facebook.com/");
+	d.manage().window().maximize();
+	TakesScreenshot ts=(TakesScreenshot)d;
+	File img=ts.getScreenshotAs(OutputType.FILE);
+	File f=new File("C:\\Users\\DELL\\eclipse-workspace\\photo\\Screenshots\\flipkart.png");
+	FileUtils.copyFile(img, f);	
+}
+}
